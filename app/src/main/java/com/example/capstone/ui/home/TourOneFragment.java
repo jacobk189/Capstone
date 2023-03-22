217016967221da53bb5233a8f807248a15c778c8
jacobk189 <jacob.krueger@snc.edu>
2023-03-22 13:01:11 -0500

    Test on laptop using different virtual device

--- app/src/main/java/com/example/capstone/ui/home/TourOneFragment.java
+++ app/src/main/java/com/example/capstone/ui/home/TourOneFragment.java
@@ -1,6 +1,11 @@
 package com.example.capstone.ui.home;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,8 +17,10 @@ import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavDirections;
 
 import com.example.capstone.BuildingDB;
 import com.example.capstone.BuildingModel;
@@ -27,12 +34,22 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 
+import java.io.IOException;
 import java.util.List;
 
 public class TourOneFragment extends Fragment {
 
     private FragmentTourOneBinding binding;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -53,20 +70,63 @@ public class TourOneFragment extends Fragment {
         BuildingDB buildingDB = new BuildingDB(TourOneFragment.this);
         List<BuildingModel> buildingList = buildingDB.showbuildings();
 
+        String apiKey = getString(R.string.google_maps_key);
 
-        //upportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_container);
+        GeoApiContext geoApiContext = new GeoApiContext.Builder().apiKey(apiKey).build();
+
         supportMapFragment.getMapAsync((new OnMapReadyCallback() {
             @Override
 
             public void onMapReady(@NonNull GoogleMap googleMap) {
                 GoogleMap mMap = googleMap;
                 LatLng location = new LatLng(buildingList.get(1).getLatitude(), buildingList.get(1).getLongitude());
-                String uri = "google.navigation:q=" + location.latitude + "," + location.longitude;
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                intent.setPackage("com.google.android.apps.maps");
-                startActivity(intent);
 
-                //LatLng location = new LatLng(buildingList.get(9).getLatitude(), buildingList.get(9).getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(location)
+                        .title("Destination");
+                googleMap.addMarker(markerOptions);
+
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is granted, get the user's current location
+                    LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                    Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    if (myLocation != null) {
+                        // Use the user's current location to set the origin of the directions request
+                        LatLng origin = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                        LatLng destination = new LatLng(buildingList.get(9).getLatitude(), buildingList.get(9).getLongitude());
+                        DirectionsApiRequest directions = DirectionsApi.newRequest(geoApiContext);
+                        directions.origin(new com.google.maps.model.LatLng(origin.latitude, origin.longitude));
+                        directions.destination(new com.google.maps.model.LatLng(destination.latitude, destination.longitude));
+                        directions.mode(TravelMode.DRIVING); // Set the travel mode to driving
+                        directions.setCallback(new PendingResult.Callback<DirectionsResult>() {
+                            @Override
+                            public void onResult(DirectionsResult result) {
+                                // Handle the directions result here
+                                // You can access the steps of the directions with result.routes[0].legs[0].steps
+                            }
+
+                            @Override
+                            public void onFailure(Throwable e) {
+                                // Handle the failure here
+                            }
+                        });
+
+                        /*try {
+                            directions.await();
+                        } catch (ApiException | InterruptedException | IOException e) {
+                            throw new RuntimeException(e);
+                        }*/
+
+                    } else {
+                        // Handle the case where the user's location is not available
+                        Log.d("null location: ", "null");
+                    }
+                } else {
+                    // Permission is not granted, request it from the user
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                }
+
                 float zoomLevel = 17.0f;
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, zoomLevel));
 
@@ -74,12 +134,6 @@ public class TourOneFragment extends Fragment {
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
-                        /*MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                        markerOptions.title(latLng.latitude+" X "+latLng.longitude);
-                        googleMap.clear();
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
-                        googleMap.addMarker((markerOptions));*/
 
                     }
                 });
