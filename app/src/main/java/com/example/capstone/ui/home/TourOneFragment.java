7eebca68795015e7c0834c4bf7ad1f8428f4cf7b
jacobk189 <jacob.krueger@snc.edu>
2023-03-28 19:30:43 -0500

    Latest Working Version.

--- app/src/main/java/com/example/capstone/ui/home/TourOneFragment.java
+++ app/src/main/java/com/example/capstone/ui/home/TourOneFragment.java
@@ -3,18 +3,15 @@ package com.example.capstone.ui.home;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -22,30 +19,24 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavDirections;
 
 import com.example.capstone.BuildingDB;
 import com.example.capstone.BuildingModel;
 import com.example.capstone.R;
-import com.example.capstone.databinding.FragmentHomeBinding;
 import com.example.capstone.databinding.FragmentTourOneBinding;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.DirectionsApi;
 import com.google.maps.DirectionsApiRequest;
 import com.google.maps.GeoApiContext;
 import com.google.maps.PendingResult;
-import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.TravelMode;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,7 +45,9 @@ public class TourOneFragment extends Fragment {
     private FragmentTourOneBinding binding;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     BluetoothAdapter ba;
-    //GoogleMap mMap = googleMap;
+    private GoogleMap mMap;
+
+    private List<LatLng> directions = null;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -84,25 +77,38 @@ public class TourOneFragment extends Fragment {
             @Override
 
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                GoogleMap mMap = googleMap;
-                LatLng location = new LatLng(buildingList.get(0).getLatitude(), buildingList.get(0).getLongitude());
-
-                MarkerOptions markerOptions = new MarkerOptions()
-                        .position(location)
-                        .title("Destination");
-                googleMap.addMarker(markerOptions);
+                mMap = googleMap;
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
                 if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     // Permission is granted, get the user's current location
+                    googleMap.setMyLocationEnabled(true);
                     LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
                     Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()), 16));
 
                     if (myLocation != null) {
                         // Use the user's current location to set the origin of the directions request
                         Log.d("In permission check:", "here");
-                        LatLng origin = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-                        LatLng destination = new LatLng(buildingList.get(9).getLatitude(), buildingList.get(9).getLongitude());
-                        getDirections(origin,destination, mMap);
+                        //LatLng origin = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                        LatLng origin = new LatLng(buildingList.get(6).getLatitude(), buildingList.get(6).getLongitude());
+                        LatLng destination = new LatLng(buildingList.get(1).getLatitude(), buildingList.get(1).getLongitude());
+                        Log.d("Your Location", destination.toString());
+                        getDirections(origin, destination, new DirectionsCallback() {
+                            @Override
+                            public void onDirectionsReceived(List<LatLng> directions) {
+                                Log.d("Directions returned", directions.toString());
+                                PolylineOptions polylineOptions = new PolylineOptions().addAll(directions).color(Color.BLUE).width(5f);
+                                Log.d("on directionsreceived", "1");
+                                googleMap.addPolyline(polylineOptions);
+                                Log.d("on directionsreceived", "2");
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 14));
+                                Log.d("on directionsreceived", "3");
+                            }
+                        });
+
+
 
                     } else {
                         // Handle the case where the user's location is not available
@@ -112,10 +118,6 @@ public class TourOneFragment extends Fragment {
                     // Permission is not granted, request it from the user
                     ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
                 }
-
-                float zoomLevel = 17.0f;
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, zoomLevel));
-
 
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
@@ -127,37 +129,57 @@ public class TourOneFragment extends Fragment {
         }));
 
 
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public interface DirectionsCallback {
+        void onDirectionsReceived(List<com.google.android.gms.maps.model.LatLng> directions);
     }
 
-    private void getDirections(LatLng origin, LatLng destination, GoogleMap mMap) {
+    private void getDirections(LatLng origin, LatLng destination, DirectionsCallback callback) {
         // Create a new instance of the Directions API
         GeoApiContext context = new GeoApiContext.Builder().apiKey(getString(R.string.google_maps_key)).build();
+
+        PolylineOptions polylineOptions = null;
 
         // Create a new Directions API request
         DirectionsApiRequest request = DirectionsApi.newRequest(context)
                 .origin(new com.google.maps.model.LatLng(origin.latitude, origin.longitude))
                 .destination(new com.google.maps.model.LatLng(destination.latitude, destination.longitude))
-                .mode(TravelMode.DRIVING);
+                .mode(TravelMode.WALKING);
 
         // Execute the request asynchronously
         request.setCallback(new PendingResult.Callback<DirectionsResult>() {
             @Override
             public void onResult(DirectionsResult result) {
                 // Draw the route on the map
-                Log.d("in onresult of get directions", ";)");
-                List<LatLng> path = new ArrayList<>();
-                for (com.google.maps.model.LatLng latLng : result.routes[0].overviewPolyline.decodePath()) {
-                    path.add(new LatLng(latLng.lat, latLng.lng));
+                Log.d("in onresult of get directions", "1");
+                if (result.routes != null && result.routes.length > 0) {
+                    Log.d("in onresult of get directions", "2");
+                    List<LatLng> path = new ArrayList<>();
+                    if (result.routes[0].overviewPolyline != null) {
+                        Log.d("in onresult of get directions", "3");
+                        List<com.google.maps.model.LatLng> decodedPath = result.routes[0].overviewPolyline.decodePath();
+                        Log.d("onResult", "decodedPath size: " + decodedPath.size());
+                        if (decodedPath != null && decodedPath.size() > 0) {
+                            Log.d("in onresult of get directions", "4");
+                            for (com.google.maps.model.LatLng latLng : decodedPath) {
+                                Log.d("in onresult of get directions", "5");
+                                path.add(new LatLng(latLng.lat, latLng.lng));
+                                Log.d("path", path.toString());
+                            }
+                        }
+                    }
+                    if (path.size() > 0) {
+                        callback.onDirectionsReceived(path); // invoke the callback with the path
+                        directions = path;
+                        Log.d("directions contents in function" , directions.toString());
+                    }
+                } else {
+                    Log.d("onResult", "Result routes is null or has no elements.");
                 }
-                PolylineOptions polylineOptions = new PolylineOptions().addAll(path).color(Color.BLUE).width(5f);
-                mMap.addPolyline(polylineOptions);
+
             }
 
             @Override
@@ -172,5 +194,12 @@ public class TourOneFragment extends Fragment {
                 });
             }
         });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
