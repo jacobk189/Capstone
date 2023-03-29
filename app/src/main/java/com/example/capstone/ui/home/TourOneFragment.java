7dd0f9bffe4dc8af138152b45ba6a5560e066201
jacobk189 <jacob.krueger@snc.edu>
2023-03-29 13:38:23 -0500

    Navigation is now working. Still need to make tour array etc...

--- app/src/main/java/com/example/capstone/ui/home/TourOneFragment.java
+++ app/src/main/java/com/example/capstone/ui/home/TourOneFragment.java
@@ -95,25 +95,19 @@ public class TourOneFragment extends Fragment {
                     if (myLocation != null) {
                         // Use the user's current location to set the origin of the directions request
                         Log.d("In permission check:", "here");
-                        //LatLng origin = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-                        LatLng origin = new LatLng(buildingList.get(6).getLatitude(), buildingList.get(6).getLongitude());
+                        LatLng origin = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
                         LatLng destination = new LatLng(buildingList.get(1).getLatitude(), buildingList.get(1).getLongitude());
-                        Log.d("Your Location", destination.toString());
-                        getDirections(origin, destination, new DirectionsCallback() {
+                        Log.d("Your Location", origin.toString());
+                        Log.d("Your destination", "Coordinates: " + destination + " Building name: " + buildingList.get(1).getName());
+
+                        getDirections(origin,destination,googleMap);
+
+                        /*getDirections(origin, destination,googleMap, new DirectionsCallback() {
                             @Override
                             public void onDirectionsReceived(List<LatLng> directions) {
                                 Log.d("Directions returned", directions.toString());
-                                List<LatLng> work = directions;
-                                /*PolylineOptions polylineOptions = new PolylineOptions().addAll(directions).color(Color.BLUE).width(5f);
-                                Log.d("on directionsreceived", "1");
-                                googleMap.addPolyline(polylineOptions);
-                                Log.d("on directionsreceived", "2");
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 14));
-                                Log.d("on directionsreceived", "3");*/
                             }
-                        });
-
-
+                        });*/
 
                     } else {
                         // Handle the case where the user's location is not available
@@ -138,11 +132,81 @@ public class TourOneFragment extends Fragment {
         return root;
     }
 
-    public interface DirectionsCallback {
-        void onDirectionsReceived(List<com.google.android.gms.maps.model.LatLng> directions);
+    private void getDirections(LatLng origin, LatLng destination, GoogleMap googleMap) {
+        // Create a new instance of the Directions API
+        GeoApiContext context = new GeoApiContext.Builder().apiKey(getString(R.string.google_maps_key)).build();
+
+        PolylineOptions polylineOptions = null;
+
+        // Create a new Directions API request
+        DirectionsApiRequest request = DirectionsApi.newRequest(context)
+                .origin(new com.google.maps.model.LatLng(origin.latitude, origin.longitude))
+                .destination(new com.google.maps.model.LatLng(destination.latitude, destination.longitude))
+                .mode(TravelMode.WALKING);
+
+        // Execute the request asynchronously
+        request.setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                // Draw the route on the map
+                Log.d("in onresult of get directions", "1");
+                if (result.routes != null && result.routes.length > 0) {
+                    Log.d("in onresult of get directions", "2");
+                    List<LatLng> path = new ArrayList<>();
+                    if (result.routes[0].overviewPolyline != null) {
+                        Log.d("in onresult of get directions", "3");
+                        List<com.google.maps.model.LatLng> decodedPath = result.routes[0].overviewPolyline.decodePath();
+                        Log.d("onResult", "decodedPath size: " + decodedPath.size());
+                        if (decodedPath != null && decodedPath.size() > 0) {
+                            Log.d("in onresult of get directions", "4");
+                            for (com.google.maps.model.LatLng latLng : decodedPath) {
+                                Log.d("in onresult of get directions", "5");
+                                path.add(new LatLng(latLng.lat, latLng.lng));
+                                Log.d("path", path.toString());
+                            }
+                        }
+                    }
+                    if (path.size() > 0) {
+                        directions = path;
+                        Log.d("directions contents in function" , directions.toString());
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                PolylineOptions polylineOptions = new PolylineOptions().addAll(path).color(Color.BLUE).width(5f);
+                                googleMap.addPolyline(polylineOptions);
+                                Log.d("on directionsreceived", "2");
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 17));
+                                Log.d("on directionsreceived", "3");
+                            }
+                        });
+                    }
+                } else {
+                    Log.d("onResult", "Result routes is null or has no elements.");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                // Handle the error here
+                Log.d("in failure of get directions", e.toString());
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), "Failed to get directions", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
     }
 
-    private void getDirections(LatLng origin, LatLng destination, DirectionsCallback callback) {
+    /*public interface DirectionsCallback {
+        void onDirectionsReceived(List<com.google.android.gms.maps.model.LatLng> directions);
+    }*/
+
+    /*private void getDirections(LatLng origin, LatLng destination, GoogleMap googleMap, DirectionsCallback callback) {
         // Create a new instance of the Directions API
         GeoApiContext context = new GeoApiContext.Builder().apiKey(getString(R.string.google_maps_key)).build();
 
@@ -180,6 +244,17 @@ public class TourOneFragment extends Fragment {
                         callback.onDirectionsReceived(path); // invoke the callback with the path
                         directions = path;
                         Log.d("directions contents in function" , directions.toString());
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                PolylineOptions polylineOptions = new PolylineOptions().addAll(path).color(Color.BLUE).width(5f);
+                                googleMap.addPolyline(polylineOptions);
+                                Log.d("on directionsreceived", "2");
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origin, 14));
+                                Log.d("on directionsreceived", "3");
+                            }
+                        });
                     }
                 } else {
                     Log.d("onResult", "Result routes is null or has no elements.");
@@ -190,7 +265,7 @@ public class TourOneFragment extends Fragment {
             @Override
             public void onFailure(Throwable e) {
                 // Handle the error here
-                Log.d("in failure of get directions", ";(");
+                Log.d("in failure of get directions", e.toString());
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -200,7 +275,7 @@ public class TourOneFragment extends Fragment {
             }
         });
 
-    }
+    }*/
 
     @Override
     public void onDestroyView() {
